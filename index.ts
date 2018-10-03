82dd6cc5a3e3d00ee7aba634e307bcf6b88b414e
Lieven Doclo <lieven@atomist.com>
2018-10-03 17:06:28 +0200

    Add strategy to index

--- index.ts
+++ index.ts
@@ -21,4 +21,7 @@ export { CommandLineCloudFoundryDeployer } from "./lib/cli/CommandLineCloudFound
 export { CloudFoundryInfo } from "./lib/api/CloudFoundryTarget";
 export { EnvironmentCloudFoundryTarget } from "./lib/config/EnvironmentCloudFoundryTarget";
 export { HasCloudFoundryManifest } from "./lib/config/cloudFoundryManifestPushTest";
-export { CloudFoundryDeploy } from "./lib/goals/CloudFoundryPushDeploy";
+export { 
+    CloudFoundryDeploy,
+    CloudFoundryDeploymentStrategy,
+} from "./lib/goals/CloudFoundryPushDeploy";
